e5810bb2b3d315c917603ef89ff28e8bc95b770e
Jaden Shelton <jaden@sheltonz.co>
2025-09-03 13:56:15 -0600

    change diagram to no longer use GoJS

--- artccDiagram.js
+++ artccDiagram.js
@@ -1,26 +1,26 @@
-        const $ = go.GraphObject.make;
+const $ = go.GraphObject.make;
 
-        const myDiagram = new go.Diagram("myDiagramDiv");
+const myDiagram = new go.Diagram("myDiagramDiv");
 
-        //adjust view window on intial load
-        const START_SCALE = 0.4;
-        myDiagram.addDiagramListener("InitialLayoutCompleted", (e) => {
-            const d = e.diagram;
-            d.zoomToFit();
-            d.scale = START_SCALE;
+//adjust view window on intial load
+const START_SCALE = 0.4;
+myDiagram.addDiagramListener("InitialLayoutCompleted", (e) => {
+    const d = e.diagram;
+    d.zoomToFit();
+    d.scale = START_SCALE;
 
-        });
+    });
 
-        //sets the Diagram image as the background
-        myDiagram.add($(go.Part,
-            {
-                layerName: "Background",
-                position: new go.Point(0, 0),
-                selectable: false,
-                pickable: false    
-            },
-            $(go.Picture,
-              "/ARTCC_ZDV.png"  
-            )
-          )
-        );
+    //sets the Diagram image as the background
+myDiagram.add($(go.Part,
+    {
+        layerName: "Background",
+        position: new go.Point(0, 0),
+        selectable: false,
+        pickable: false    
+    },
+    $(go.Picture,
+        "/ARTCC_ZDV.png"  
+    )
+  )
+);
